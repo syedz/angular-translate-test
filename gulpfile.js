79abf0ff830303b3fec43f668752813ed9d4a92c
Zeeshan Syed <zee@percy3d.com>
2016-10-24 15:49:50 -0400

    Created translations for Arabic

--- gulpfile.js
+++ gulpfile.js
@@ -11,11 +11,22 @@ gulp.task('js', function(){
   return gulp.src([
     './bower_components/angular/angular.js',
     './bower_components/angular-translate/angular-translate.js',
+    './bower_components/angular-translate-handler-log/angular-translate-handler-log.js',
+    './bower_components/angular-translate-loader-static-files/angular-translate-loader-static-files.js',
+    './bower_components/angular-cookies/angular-cookies.js',
+    './bower_components/angular-translate-storage-cookie/angular-translate-storage-cookie.js',
+    './bower_components/angular-translate-storage-local/angular-translate-storage-local.js',
 
     './js/app.js'])
     .pipe(concat('app.min.js'))
-    .pipe(uglify())
+    // .pipe(uglify())
     .pipe(gulp.dest('./js'))
+});
+
+gulp.task('sass', function () {
+  return gulp.src(['./sass/ltr-app.scss', './sass/rtl-app.scss'])
+    .pipe(sass())
+    .pipe(gulp.dest('./css'));
 });
 
 gulp.task('serve', function() {
@@ -27,7 +38,7 @@ gulp.task('serve', function() {
 });
 
 gulp.task('build', [], function() {
-  runSequence('js');
+  runSequence('js', 'sass');
 });
 
 gulp.task('default', ['build'], function() {});
